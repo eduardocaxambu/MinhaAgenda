2083ae03afb4912e8be9bba988f935baa3e6a188
eduardocaxambutradutor@gmail.com <Eduardo>
2022-08-25 16:58:52 -0300

    Initial commit

---
+++ app/src/main/java/com/eduardocaxambu/minhaagenda/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.eduardocaxambu.minhaagenda
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun chama(view: View) {
+        // val MainActivity2
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
